3b098a83f1461cd66a85ba1a1bef1a83f3e0672c
Antonio Queb <cardstillo@gmail.com>
2026-06-26 10:58:57 -0600

    Actualizar versión a 19.0.1.4.0 y mejorar el diseño del informe de recibo de pago en efectivo

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Comprobante de Pago en Efectivo',
-    'version': '19.0.1.3.1',
+    'version': '19.0.1.4.0',
     'category': 'Sales',
     'summary': 'Genera recibos de pago en efectivo desde órdenes de venta',
     'description': """
