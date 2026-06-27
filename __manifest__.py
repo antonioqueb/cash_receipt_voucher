ca3dd266817daa2014c6804a6e37cb1de15e7932
Antonio Queb <cardstillo@gmail.com>
2026-06-26 22:46:07 -0600

    feat: Actualizar versión del módulo a 19.0.3.1.3 y mejorar el diseño responsivo del dashboard

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Comprobante de Pago en Efectivo',
-    'version': '19.0.3.1.2',
+    'version': '19.0.3.1.3',
     'category': 'Sales',
     'summary': 'Genera recibos de pago en efectivo desde órdenes de venta',
     'description': """
